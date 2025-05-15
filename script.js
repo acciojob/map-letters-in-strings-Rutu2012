0bc993f793da416abeaaaefe553969f206891119
Rutu2012 <sanghanirutu944@gmail.com>
2025-05-15 11:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+  
+  for (let i = 0; i < word.length; i++) {
+    const char = Symbol.for(word[i]); // Use global symbol registry
+    if (!result[char]) {
+      result[char] = [];
+    }
+    result[char].push(i);
+  }
+
+  return result;
+}
+console.log(mapLetters("dodo"));
+console.log(mapLetters("froggy"));
+
